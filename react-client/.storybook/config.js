8fb74607ad98815f6e5e1340c011615a4cb1e109
Jerome <jeromess.88@gmail.com>
2018-10-21 21:37:52 +0100

    Set up Storybook info addon and importing bootstrap in order to use it on the stories

--- react-client/.storybook/config.js
+++ react-client/.storybook/config.js
@@ -1,9 +1,31 @@
 import { configure } from '@storybook/react';
-
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {setDefaults} from "@storybook/addon-info";
 // automatically import all files ending in *.stories.js
 const req = require.context('../src', true, /.stories.js$/);
 function loadStories() {
   req.keys().forEach(filename => req(filename));
 }
+
+// Setting up info-addon
+setDefaults({
+    styles: {
+        infoBody: {
+            border: 'none',
+            boxShadow: 'none',
+            padding: '20px',
+            margin: '0'
+        },
+        infoStory: {
+          margin: '0',
+          padding: '0 20px'
+        },
+        jsxInfoContent: {
+          padding: '20px'
+        }
+    },
+    inline: true,
+    maxPropsIntoLine: 0
+});
 
 configure(loadStories, module);
